0d85b40918b52938466823b0359612b02e1d34ce
redCat <yana.sitkowecz@yandex.ru>
2025-05-23 09:54:13 +0300

    add DataExporter class

--- src/main/java/org/acme/foodpackaging/app/FoodPackagingApp.java
+++ src/main/java/org/acme/foodpackaging/app/FoodPackagingApp.java
@@ -6,24 +6,19 @@ import ai.timefold.solver.core.api.solver.SolutionManager;
 import ai.timefold.solver.core.api.solver.Solver;
 import ai.timefold.solver.core.api.solver.SolverFactory;
 import ai.timefold.solver.core.config.solver.SolverConfig;
+import org.acme.foodpackaging.bootstrap.DataExporter;
 import org.acme.foodpackaging.bootstrap.ImportOrderData;
-import org.acme.foodpackaging.bootstrap.JsonExporter;
 import org.acme.foodpackaging.domain.Job;
 import org.acme.foodpackaging.domain.Line;
 import org.acme.foodpackaging.domain.PackagingSchedule;
 import org.acme.foodpackaging.solver.FoodPackagingConstraintProvider;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 public class FoodPackagingApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         if(args.length!=0) {
             SolverFactory<PackagingSchedule> solverFactory = SolverFactory.create(new SolverConfig()
@@ -37,31 +32,18 @@ public class FoodPackagingApp {
             ImportOrderData importData = new ImportOrderData();
 
             PackagingSchedule problem = importData.scheduleInitializer(args[0]);
+
+            DataExporter exporter = new DataExporter(args[0]);
+            exporter.exportAsYaml(problem);
             // Solve the problem
             Solver<PackagingSchedule> solver = solverFactory.buildSolver();
             PackagingSchedule solution = solver.solve(problem);
 
             SolutionManager< PackagingSchedule, HardMediumSoftScore> solutionManager = SolutionManager.create(solverFactory);
             ScoreExplanation< PackagingSchedule, HardMediumSoftScore> scoreExplanation = solutionManager.explain(solution);
-          DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-            JsonExporter exporter = new JsonExporter(String.valueOf(importData.getDATE().format(formatter)), problem.getJobs());
-
-            String exportFileDefaultPath = "src/main/resources/exportFiles/" + args[0] + "Export.json";
-            if (args.length > 1) {
-                try {
-                    Path customExportPath = Paths.get(args[1]);
-                    Files.createDirectories(customExportPath.getParent()); // создаёт все нужные директории
-                    exporter.convertToJsonFile(String.valueOf(customExportPath));
-                } catch (IOException e) {
-                    System.err.println("Ошибка записи: " + e.getMessage());
-                    exporter.convertToJsonFile(exportFileDefaultPath);
-                }
-            }
-
-            else {
-                exporter.convertToJsonFile(exportFileDefaultPath);
-            }
+            exporter.exportAsJson(problem);
+            
         }
         else {
             System.out.println("No date set!");
---
+++ src/main/java/org/acme/foodpackaging/bootstrap/DataExporter.java
@@ -0,0 +1,155 @@
+package org.acme.foodpackaging.bootstrap;
+
+import ai.timefold.solver.core.api.score.buildin.hardmediumsoft.HardMediumSoftScore;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.acme.foodpackaging.domain.Job;
+import org.acme.foodpackaging.domain.Line;
+import org.acme.foodpackaging.domain.PackagingSchedule;
+import org.acme.foodpackaging.domain.WorkCalendar;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class DataExporter {
+
+    private HardMediumSoftScore totalScore;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private String date;
+    public DataExporter(String date){
+       this.date =date;
+    }
+
+    public void exportAsJson(PackagingSchedule problem){
+        Map<String, Object> jsonMap = new HashMap<>();
+        List<JobRecord> jobList = getJobList(problem.getJobs(), problem.getLines());
+        jsonMap.put("DATE", date);
+        jsonMap.put("Job", jobList);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        String filePath = "src/main/resources/exportFiles/" + this.date + "Export.json";
+        File file = new File(filePath);
+
+        try {
+            // Создаем все необходимые родительские директории
+            File parentDir = file.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, jsonMap);
+            System.out.println("JSON файл успешно создан: " + filePath);
+        } catch (IOException e) {
+            System.err.println("Ошибка при создании JSON файла: " + e.getMessage());
+        }
+    }
+
+    public void exportAsYaml(PackagingSchedule problem) {
+        Map<String, Object> yamlMap = new HashMap<>();
+        WorkCalendar calendar = problem.getWorkCalendar();
+        yamlMap.put("WorkCalendar", calendar.toString());
+        yamlMap.put("Products", problem.getProducts());
+        yamlMap.put("Jobs", problem.getJobs());
+        yamlMap.put("Lines", problem.getLines());
+
+        String filePath = "src/main/resources/importFiles/" + this.date + "Import.yaml";
+
+        ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+        yamlMapper.registerModule(new JavaTimeModule());
+        yamlMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        File file = new File(filePath);
+
+        try {
+            // Создаем все необходимые родительские директории
+            File parentDir = file.getParentFile();
+            if (parentDir != null && !parentDir.exists()) {
+                parentDir.mkdirs();
+            }
+            yamlMapper.writeValue(new File(filePath),yamlMap);
+            System.out.println("YAML файл успешно создан: " + filePath);
+        } catch (IOException e) {
+            System.err.println("Ошибка при создании JSON файла: " + e.getMessage());
+        }
+    }
+
+    private List<JobRecord> getJobList(List<Job> jobs, List<Line> lines){
+
+        List<JobRecord> jobRecordList = new ArrayList<>(jobs.size());
+        for(Job job : jobs){
+            String lineName = lines.stream()
+                    .filter(line -> line.getJobs().contains(job))
+                    .map(Line::getName)
+                    .findFirst()
+                    .orElse("UNASSIGNED");
+
+            jobRecordList.add(new JobRecord(
+                    job.getId(),
+                    job.getName(),
+                    job.getProduct().getType().getDisplayName(),
+                    job.getProduct().getGlaze().getDisplayName(),
+                    String.valueOf(job.getQuantity()),
+                    lineName,
+                    String.valueOf(job.getDuration()))
+            );
+        }
+        return jobRecordList;
+    }
+
+    @JsonPropertyOrder({"ID", "Job","ProductType", "Glaze", "Quantity", "Line", "Duration"})
+    @JsonFormat(shape = JsonFormat.Shape.OBJECT)
+    record JobRecord (String id, String jobName, String type, String glaze, String quantity, String line, String duration) {
+
+        @Override
+        @JsonProperty("ID")
+        public String id() {
+            return id;
+        }
+
+        @Override
+        @JsonProperty("Job")
+        public String jobName() {
+            return jobName;
+        }
+
+        @Override
+        @JsonProperty("ProductType")
+        public String type() {
+            return type;
+        }
+
+        @Override
+        @JsonProperty("Glaze")
+        public String glaze() {
+            return glaze;
+        }
+        @Override
+        @JsonProperty("Quantity")
+        public String quantity() {
+            return quantity;
+        }
+
+        @Override
+        @JsonProperty( "Line")
+        public String line() {
+            return line;
+        }
+
+        @Override
+        @JsonProperty( "Duration")
+        public String duration() {
+            return duration;
+        }
+    }
+}
